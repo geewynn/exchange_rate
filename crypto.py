82b3eb614e7c529cfc4333ea550da39a36bc9abb
geewynn <ekainugodwin@gmail.com>
2024-01-08 18:38:38 +0100

    extract data to postgres

--- crypto.py
+++ crypto.py
@@ -1,6 +1,31 @@
 import requests
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+import sqlalchemy
+load_dotenv()
+
+# load env variables
+db_host = os.environ["INSTANCE_HOST"]
+db_user = os.environ["POSTGRES_USER"]
+db_pass = os.environ["POSTGRES_PW"]
+db_name = os.environ["POSTGRES_DB"]
+db_port = os.environ["POSTGRES_PORT"]
+
+
+top_ten = ['bitcoin', 'ethereum', 'tether', 'binancecoin', 'solana', 'ripple','usd-coin', 'staked-ether','cardano', 'avalanche-2']
+connection_str = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+# SQLAlchemy engine
+engine = create_engine(connection_str)
+
+try:
+    with engine.connect() as connection_str:
+        print('Successfully connected to the PostgreSQL database')
+except Exception as ex:
+    print(f'Sorry failed to connect: {ex}')
 
 def fetch_token_data(coin):
     url = f'https://api.coingecko.com/api/v3/coins/{coin}/market_chart?vs_currency=usd&days=7'
@@ -12,11 +37,11 @@ def fetch_token_data(coin):
         data = response.json()
         return data['prices']
     else:
-        print(f'Failed to fetch data for {token}. Status code: {response.status_code}')
+        print(f'Failed to fetch data for {coin}. Status code: {response.status_code}')
         return []
 
+
 # List of top ten tokens
-top_ten = ['bitcoin', 'ethereum', 'tether', 'binancecoin', 'solana', 'ripple','usd-coin', 'staked-ether','cardano', 'avalanche-2']
 exchange_rate = []
 
 # Fetch and print hourly exchange rate data for each token
@@ -32,6 +57,7 @@ for token in top_ten:
     else:
         print(f'No data available for {token.capitalize()}')
 rates = pd.concat(exchange_rate, ignore_index=True)
-rates.to_csv('token.csv', index=False)
+# rates.to_csv('token.csv', index=False)
 
+rates.to_sql('exch', con=engine, if_exists="replace", index=False)
 
